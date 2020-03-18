e46db1f34cb4de07584f51b5ec45eff096c0581a
github username <github email address>
2020-03-18 17:53:44 -0400

    whatever

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,2 +1,23 @@
 class DogsController < ApplicationController
+
+    def index
+        @dogs = Dog.all
+    end
+
+    def show
+        @dog = Dog.find(params[:id])
+    end
+
+    def new
+
+    end
+
+    def create
+
+    end
+
+    def increment_like
+        @dog = Dog.find(params[:id])
+        @dog.likes += 1
+    end
 end
--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,2 +1,32 @@
 class EmployeesController < ApplicationController
+
+    def index
+        @employees = Employee.all
+    end
+
+    def show
+        @employee = Employee.find(params[:id])
+    end
+    
+    def new
+        @employee = Employee.new
+        @dogs = Dog.all
+    end
+
+    def create
+        @employee = Employee.create(employee_params)
+
+        if @employee.valid? 
+            redirect_to @employee
+        else
+            flash[:errors] = @employee.errors.full_messages
+            redirect_to new_employee_path
+        end
+    end
+
+    private
+
+    def employee_params
+        params.require(:employee).permit!
+    end
 end
